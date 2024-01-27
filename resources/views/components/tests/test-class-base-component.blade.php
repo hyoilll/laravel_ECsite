fd48b9b64ce82a497a9c96356537a7e9a76d968a
hyoil Lee <dlgydlf123@gmail.com>
2024-01-27 17:53:35 +0900

    classBase

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    クラスベースのコンポーネント
+    <div>{{ $classBaseMessage }}</div>
+    <div>{{ $defaultMessage }}</div>
+    <!-- I begin to speak only when I am certain what I will say is not better left unsaid. - Cato the Younger -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,9 @@
 <!-- resources/views/components　配下のコンポーネントファイルを使用する -->
 <x-tests.app>
-compoent-test2
+    <x-slot name="header">header2</x-slot>
+
+    compoent-test2
+    <x-test-class-base classBaseMessage="メッセージです"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています。"/>
 </x-tests.app>
